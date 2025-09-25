a607c3da0f1a4a83340e911d30a4ffabe364487c
SrHenry <lucapvh949@gmail.com>
2025-09-25 17:13:37 -0400

    fix: 🚑 incorrect metadata schema and add debug logs in extractYTContentID function

--- src/functions/extractYTContentID.ts
+++ src/functions/extractYTContentID.ts
@@ -1,9 +1,10 @@
-import { asNull, object, or, string } from "@srhenry/type-utils";
+import { error } from "@/log/index.ts";
+import { asNull, Experimental, object, or, string } from "@srhenry/type-utils";
 import { runYtDlp } from "./runYtDlp.ts";
 
 const isMetadata = object({
     id: string(),
-    playlist_id: or(string(), asNull()),
+    playlist_id: or.optional(string(), asNull()),
 });
 const isEmptyArray = <T = unknown>(arr: Array<T>) => arr.length === 0;
 const hasOneElement = <T = unknown>(arr: Array<T>) => arr.length === 1;
@@ -31,8 +32,20 @@ export async function extractYTContentID(yt_src: string) {
     if (isEmptyArray(j))
         throw new Error("Could not retrieve JSON from yt-dlp.");
 
-    if (!j.every(isMetadata))
+    if (!j.every(isMetadata)) {
+        const validationErrors = Experimental.validate(
+            j[0],
+            isMetadata,
+            false
+        ) as Experimental.ValidationErrors;
+
+        error(
+            "YouTube source's metadata is not compliant with defined schema!\n %s",
+            validationErrors.toString()
+        );
+
         throw new TypeError("Unexpected medatata value. Missing properties.");
+    }
 
     if (hasOneElement(j)) return j[0].id;
 
