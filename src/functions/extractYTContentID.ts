d8d55871f974056b45cab4e734afeefaf013cf1b
SrHenry <lucapvh949@gmail.com>
2025-10-31 16:52:40 -0400

    refactor: :recycle: reorganize metadata validation and parsing logic in extractYTContentID function

--- src/functions/extractYTContentID.ts
+++ src/functions/extractYTContentID.ts
@@ -2,21 +2,55 @@ import {
     array,
     asNull,
     Experimental,
+    type GetTypeGuard,
     object,
     or,
     string,
 } from "@srhenry/type-utils";
 
 import { runYtDlp } from "@/functions/runYtDlp.ts";
+import { error, info } from "@/log/index.ts";
 
 //TODO: Refactor and abstract this file to more modules
 
-const isMetadata = object({
-    id: string(),
-    playlist_id: or.optional(string(), asNull()),
-});
+const MetadataSchema = () =>
+    object({
+        id: string(),
+        playlist_id: or.optional(string(), asNull()),
+    });
+const MetadataArraySchema = () => array(MetadataSchema());
+
 const isEmptyArray = <T = unknown>(arr: Array<T>) => arr.length === 0;
 const hasOneElement = <T = unknown>(arr: Array<T>) => arr.length === 1;
+
+type Metadata = GetTypeGuard<ReturnType<typeof MetadataSchema>>;
+type MetadataValidationError = Experimental.ValidationError<
+    unknown,
+    Metadata[]
+>;
+
+const validateMetadata = (
+    metadata: unknown
+): Experimental.Result<
+    Metadata[],
+    Experimental.ValidationErrors<MetadataValidationError[]>
+> => {
+    const result = Experimental.validate(
+        metadata,
+        MetadataArraySchema(),
+        false
+    );
+
+    return result instanceof Experimental.ValidationErrors
+        ? [
+              <Experimental.ValidationErrors<MetadataValidationError[]>>result,
+              null,
+          ]
+        : [null, result];
+};
+
+const parseOutputAsJson = (output: string) =>
+    output.split("\n").map((row) => JSON.parse(row));
 
 /**
  * It fetches the YouTube ID from a YouTube source
@@ -34,29 +68,35 @@ export async function extractYTContentID(yt_src: string) {
         "-j",
     ];
 
-    const j = await runYtDlp(...args, yt_src).then((o) =>
-        o.split("\n").map((row) => JSON.parse(row))
-    );
+    info("[progress] [fn:%s] Fetching metadata...", runYtDlp.name);
 
-    if (isEmptyArray(j))
+    const output = await runYtDlp(...args, yt_src);
+    const metadata = parseOutputAsJson(output);
+
+    info("[progress] [fn:%s] Validating metadata...", isEmptyArray.name);
+
+    if (isEmptyArray(metadata))
         throw new Error("Could not retrieve JSON from yt-dlp.");
 
-    if (!j.every(isMetadata)) {
-        const validationErrors = Experimental.validate(
-            j[0],
-            isMetadata,
-            false
-        ) as Experimental.ValidationErrors;
+    info("[progress] [fn:%s] Validating metadata...", validateMetadata.name);
 
+    const [err, validatedMetadata] = validateMetadata(metadata);
+
+    if (err) {
         error(
             "YouTube source's metadata is not compliant with defined schema!\n %s",
-            validationErrors.toString()
+            err.toString()
         );
 
         throw new TypeError("Unexpected medatata value. Missing properties.");
     }
 
-    if (hasOneElement(j)) return j[0].id;
+    info(
+        "[progress] [fn:%s] Metadata validated sucessfully! Fetching ID/Playlist ID...",
+        hasOneElement.name
+    );
 
-    return j[0].playlist_id!;
+    if (hasOneElement(validatedMetadata)) return validatedMetadata[0].id;
+
+    return validatedMetadata[0].playlist_id!;
 }
