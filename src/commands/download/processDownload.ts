7138ca3ed9d561e6c3a9ebc4379459e15365def4
Luís Henrique da Silva Santos <55113620+SrHenry@users.noreply.github.com>
2025-12-25 16:21:27 -0400

    fix: add continue at if options.playlist in for loop
    Previous commits at this fix branch removed returns but by just removing it the alternative branch executes right after the playlist one, instead of being one or another.

--- src/commands/download/processDownload.ts
+++ src/commands/download/processDownload.ts
@@ -5,14 +5,17 @@ import { DownloadOptions } from "@/workflow/pipelines/download/music/types/Downl
 export async function processDownload(
     sources: string[],
     options: DownloadOptions
-) {
+): Promise<void> {
     for (const [source, c] of sources.map((s, i) => [s, i + 1] as const)) {
         const run = () =>
             downloadMusicPipeline(source, options).catch((err) =>
                 console.error(`Error while processing playlist:`, err)
             );
 
-        if (options.playlist) await run();
+        if (options.playlist) {
+            await run();
+            continue
+        }
 
         await printProcessingEntry(c, sources.length, run);
     }
