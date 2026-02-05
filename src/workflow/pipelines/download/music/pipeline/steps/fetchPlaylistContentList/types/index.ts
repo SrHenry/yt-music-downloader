fdae1f1d9f5b3fd88f23c9287fcd61d937e874b0
SrHenry <lucapvh949@gmail.com>
2026-02-04 22:00:10 -0400

    style: ✏ normalize relative import path to virtual path

--- src/workflow/pipelines/download/music/pipeline/steps/fetchPlaylistContentList/types/index.ts
+++ src/workflow/pipelines/download/music/pipeline/steps/fetchPlaylistContentList/types/index.ts
@@ -1,8 +1,5 @@
-import type { Output as PreviousOutput } from "@/workflow/pipelines/download/music/pipeline/steps/validateSource/types/index.ts";
 import type { YouTubePlaylistMetadata } from "@/workflow/pipelines/download/music/types/YouTubePlaylistMetadata.ts";
 import type { ValidationError, ValidationErrors } from "@srhenry/type-utils";
-
-export type Input = PreviousOutput;
 
 export type Input = string;
 
